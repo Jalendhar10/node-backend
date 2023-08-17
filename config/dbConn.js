25e2aa102557827c248c581f4a4768f09ec691ba
Jalendhar10 <56975828+Jalendhar10@users.noreply.github.com>
2023-08-17 10:58:51 +0530

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI)
+        await mongoose.connect(process.env.mongodb+srv://jeli:Jalendhar@10@cluster0.auqumpa.mongodb.net/?retryWrites=true&w=majority)
     } catch (err) {
         console.log(err)
     }
